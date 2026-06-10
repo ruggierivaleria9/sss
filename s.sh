328a85c06f6c8fe2547727caeb7977585ffbfa73
ruggierivaleria9 <ruggierivaleria9@gmail.com>
2026-06-10 10:19:30 +0200

    Update s.sh

--- s.sh
+++ s.sh
@@ -6,4 +6,4 @@ mkdir -p /app/site /app/risultati/DATA_SPLIT /app/logs /app/whoisds
 [ -d /opt/site ] && cp -r /opt/site/* /app/site/
 [ -d /opt/risultati ] && cp -r /opt/risultati/* /app/risultati/
 
-exec node /opt/s/s.bundle.js
+exec node /opt/scanner/scanner.bundle.js
